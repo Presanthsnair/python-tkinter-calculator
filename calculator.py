86938efe48e89a458e4d86767c3e569d3c26077e
presanth s nair <presanthsnair2000@gmail.com>
2020-07-16 17:51:47 +0530

    displsying input field  using  grid

--- calculator.py
+++ calculator.py
@@ -1,8 +1,11 @@
 from tkinter import *
+from tkinter import Button
 
 window = Tk()
 window.geometry("500x500")  # window size
-display = Entry(window)  # input field
 
-display.pack()
+display = Entry(window)  # input field
+display.grid(row=2,column=20)
+
+
 window.mainloop()
