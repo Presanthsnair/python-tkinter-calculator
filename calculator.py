45d0899ed762b4b93423515fcccffadb3efb477b
presanth s nair <presanthsnair2000@gmail.com>
2020-07-19 17:01:21 +0530

    fully removed grid and added 'place'

--- calculator.py
+++ calculator.py
@@ -1,51 +1,53 @@
 from tkinter import *
 
 window = Tk()
-window.geometry("500x500")  # window size
+window.geometry("350x450")  # window size
 window.configure(bg="red")
 
-display = Entry(window, width=80, bg="black", fg="white")  # input field
-display.grid(row=0, column=0)
+display = Entry(window, width=35,bg="black", fg="white",font=90)  # input field
+display.place(x=15,y=7)
 
 def cal(val):
     display.insert(0,val)
+def clr():
+    display.delete(0,END)
 
+clr_button=Button(window, text="CLR", command=lambda :clr(), width=10, height=3, bg="white", activebackground="light blue")
+button1 = Button(window, text="7", command=lambda :cal(7), width=7, height=3, bg="white", activebackground="light blue")
+button2 = Button(window, text="8", command=lambda :cal(8), width=7, height=3, bg="white", activebackground="light blue")
+button3 = Button(window, text="9", command=lambda :cal(9), width=7, height=3, bg="white", activebackground="light blue")
+button4 = Button(window, text="X", command=lambda :cal("*"),  width=7, height=3, bg="white", activebackground="light blue")
+button5 = Button(window, text="4", command=lambda :cal(4),  width=7, height=3, bg="white", activebackground="light blue")
+button6 = Button(window, text="5", command=lambda :cal(5),  width=7, height=3, bg="white", activebackground="light blue")
+button7 = Button(window, text="6", command=lambda :cal(6),  width=7, height=3, bg="white", activebackground="light blue")
+button8 = Button(window, text="-", command=lambda :cal("-"), width=7, height=3, bg="white", activebackground="light blue")
+button9 = Button(window, text="1", command=lambda :cal(1),  width=7, height=3, bg="white", activebackground="light blue")
+button10 = Button(window, text="2", command=lambda :cal(2),  width=7, height=3, bg="white", activebackground="light blue")
+button11 = Button(window, text="3", command=lambda :cal(3), width=7, height=3, bg="white", activebackground="light blue")
+button12 = Button(window, text="+", command=lambda :cal("+"), width=7, height=3, bg="white", activebackground="light blue")
+button13 = Button(window, text="/", command=lambda :cal("/"),  width=7, height=3, bg="white", activebackground="light blue")
+button14 = Button(window, text="0", command=lambda :cal(0),  width=7, height=3, bg="white", activebackground="light blue")
+button15 = Button(window, text=".", command=lambda :cal("."),  width=7, height=3, bg="white", activebackground="light blue")
+button16 = Button(window, text="=", command=lambda :cal("="), width=7, height=3, bg="white", activebackground="light blue")
 
-button1 = Button(window, text="7", command=lambda :cal(7), width=10, height=5, bg="white", activebackground="light blue")
-button2 = Button(window, text="8", command=lambda :cal(8), width=10, height=5, bg="white", activebackground="light blue")
-button3 = Button(window, text="9", command=lambda :cal(9), width=10, height=5, bg="white", activebackground="light blue")
-button4 = Button(window, text="X", command=lambda :cal(X), width=10, height=5, bg="white", activebackground="light blue")
-button5 = Button(window, text="4", command=lambda :cal(4), width=10, height=5, bg="white", activebackground="light blue")
-button6 = Button(window, text="5", command=lambda :cal(5), width=10, height=5, bg="white", activebackground="light blue")
-button7 = Button(window, text="6", command=lambda :cal(6), width=10, height=5, bg="white", activebackground="light blue")
-button8 = Button(window, text="-", command=lambda :cal("-"), width=10, height=5, bg="white", activebackground="light blue")
-button9 = Button(window, text="1", command=lambda :cal(1), width=10, height=5, bg="white", activebackground="light blue")
-button10 = Button(window, text="2", command=lambda :cal(2), width=10, height=5, bg="white", activebackground="light blue")
-button11 = Button(window, text="3", command=lambda :cal(3), width=10, height=5, bg="white", activebackground="light blue")
-button12 = Button(window, text="+", command=lambda :cal("+"), width=10, height=5, bg="white", activebackground="light blue")
-button13 = Button(window, text="/", command=lambda :cal("/"), width=10, height=5, bg="white", activebackground="light blue")
-button14 = Button(window, text="0", command=lambda :cal(0), width=10, height=5, bg="white", activebackground="light blue")
-button15 = Button(window, text=".", command=lambda :cal("."), width=10, height=5, bg="white", activebackground="light blue")
-button16 = Button(window, text="=", command=lambda :cal("="), width=10, height=5, bg="white", activebackground="light blue")
+clr_button.place(x=15,y=40)
+button1.place(x=15,y=110)
+button2.place(x=95,y=110)
+button3.place(x=170,y=110)
+button4.place(x=245,y=110)
 
-button1.grid(row=1, column=2)
-button2.grid(row=1, column=4)
-button3.grid(row=1, column=6)
-button4.grid(row=1, column=8)
+button5.place(x=15,y=190)
+button6.place(x=95,y=190)
+button7.place(x=170,y=190)
+button8.place(x=245,y=190)
 
-button5.grid(row=3, column=2)
-button6.grid(row=3, column=4)
-button7.grid(row=3, column=6)
-button8.grid(row=3, column=8)
+button9.place(x=15,y=270)
+button10.place(x=95,y=270)
+button11.place(x=170,y=270)
+button12.place(x=245,y=270)
 
-button9.grid(row=4, column=2)
-button10.grid(row=4, column=4)
-button11.grid(row=4, column=6)
-button12.grid(row=4, column=8)
-
-button13.grid(row=6, column=2)
-button14.grid(row=6, column=4)
-button15.grid(row=6, column=6)
-button16.grid(row=6, column=8)
-
+button13.place(x=15,y=350)
+button14.place(x=95,y=350)
+button15.place(x=170,y=350)
+button16.place(x=245,y=350)
 window.mainloop()
